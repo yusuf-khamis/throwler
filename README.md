c9bebd13d9626fb403d4f021a25eb200cd67e156
Yusuf Khamis <yusufkhamis27@gmail.com>
2020-02-22 04:37:36 +0300

    Fixed tests, pending tests for after process.nextTick callback

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Node.JS simplistic logger that auto rotates logs
 
 Throwler was designed with the intention of making it extremely easy to log content from a NodeJS app with the feature of auto rotating files.
 
+*I made this library because I wanted to have the logging feature on my app and I felt like none of the libraries did it how I wanted.*
+
 ## Why throwler
 
 1. Its extremely light
@@ -20,12 +22,12 @@ $ npm install throwler
 ## Options
 
 * **output:** Specifies where to put the logs. Accepts `console` and `file` to either output to console or file respectively. Default is `file`.
-* **levels:** Specifies the levels array in which to classify the logs in, these are not restricted to any values but only alpha characters allowed. Default is `[]`.
 * **timestamp:** Whether or not to include timestamp in the logs. Default is `true`;
 * **autorotate:** Specifies the frequency in which to rotate logs. Accepts `hourly`, `daily`, `none`. If `none` is specified, no logs will still be saved in the display name date format but will not be folderised. Default is `hourly`.
 * **dirDateFormat** This sets the  date format for which to be used to name the folders when rotating logs. Accepts same formatting options as moment but only for date, month and year (DMY) and separators (-_., \/). Default is `DD-MMMM-YYYY`.
 * **dateFormat** This sets the date format for naming the log files. Accepts same formatting options as moment. All log files have extension `.log`. When level specified e.g. _error_, log file will be _12-December-2018_error.log_ or _12-December-2018.log_ if level not specified. Default is `DD-MMMM-YYYYTHH`.
 * **dir** This sets the location in which to store the logs (files or folders). Default is `.`.
+* **size** This the size limit for each file after which another file will be created on logging. Values can be in gigabytes (`G`), megabytes (`M`) or kilobytes (`K`). The units are case insensitive. Default is `10M`
 
 ## Usage
 
@@ -36,21 +38,17 @@ const path = require('path');
 
 const log = logger({
     output: 'file',
-    levels: [
-        'error',
-        'warning',
-        'info'
-    ],
     timestamp: true,
     autorotate: 'hourly',
     dirDateFormat: 'DD-MMMM-YYYY',
     dateFormat: 'DD-MMMM-YYYYTHH',
-    dir: path.resolve(__dirname, 'logs')
+    dir: path.resolve(__dirname, 'logs'),
+    size: '10M'
 });
 
-log('Hello throwler with level', 'info'); //NB: If this level is not specified in levels option then it will be discarded
+log('Hello throwler with level', 'info'); //NB: This level will just classify the logs into their separate intended content
 
-log('Hello throwler without level');
+log('Hello throwler without level'); // NB: All logs in one autorotated file file
 
 ```
 
--- test.js
+++ test.js
@@ -1,8 +1,9 @@
-const sinon = require('sinon')
 const fs = require('fs')
+const glob = require('glob')
 const path = require('path')
-const moment = require('moment')
-const crypto = require('crypto')
+
+jest.mock('fs')
+jest.mock('glob')
 
 const { createLogger } = require('.')
 
@@ -29,15 +30,7 @@ describe('Testing initializing logger to return a logging function', () => {
 })
 
 describe('Testing console log output logs', () => {
-  let consoleLogStub
-
-  beforeEach(() => {
-    consoleLogStub = sinon.stub(console, 'log')
-  })
-
-  afterEach(() => {
-    consoleLogStub.restore()
-  })
+  jest.spyOn(console, 'log')
 
   it('should call console.log function if output is console', () => {
     const logger = createLogger({
@@ -46,170 +39,239 @@ describe('Testing console log output logs', () => {
 
     logger('testing logger console output')
 
-    expect(consoleLogStub.called).toBe(true)
+    expect(console.log).toBeCalled()
   })
 })
 
 describe('Testing write stream function to output logs to a file', () => {
-  let createStreamStub
-  let fsMkdirSyncStub
+  const writeStreamStub = jest.fn()
+  const onceStreamEventStub = jest.fn()
+  const endStreamStub = jest.fn()
+  const onStreamStub = jest.fn()
 
-  let writeStreamStub
-  let onceStreamEventStub
-  let endStreamStub
-  let onStreamStub
+  const isFileStub = jest.fn()
 
-  beforeEach(() => {
-    writeStreamStub = sinon.stub()
-    onceStreamEventStub = sinon.stub()
-    endStreamStub = sinon.stub()
-    onStreamStub = sinon.stub()
-
-    createStreamStub = sinon.stub(fs, 'createWriteStream').returns({
-      write: writeStreamStub,
-      once: onceStreamEventStub,
-      end: endStreamStub,
-      on: onStreamStub
-    })
-
-    fsMkdirSyncStub = sinon.stub(fs, 'mkdirSync')
+  fs.createWriteStream.mockReturnValue({
+    write: writeStreamStub,
+    once: onceStreamEventStub,
+    end: endStreamStub,
+    on: onStreamStub
   })
 
-  afterEach(() => {
-    createStreamStub.restore()
-    fsMkdirSyncStub.restore()
+  fs.lstatSync.mockReturnValue({
+    isFile: isFileStub
   })
 
   it('logging with file output should create the log folder if it does not exists', () => {
+    fs.existsSync.mockReturnValueOnce(false)
+    fs.existsSync.mockReturnValueOnce(false)
+    fs.existsSync.mockReturnValueOnce(true)
+
     createLogger({
-      output: 'file',
-      dir: path.join(__dirname, 'logs')
-    })
-
-    expect(fsMkdirSyncStub.called).toBe(true)
-  })
-
-  it('logging with file output but with non-existent folder and parent folder should throw an error', () => {
-    expect(() => createLogger({
-      output: 'file',
-      dir: path.join(__dirname, 'parent', 'log')
-    })).toThrow()
-  })
-
-  it('logging with file output and with dir specified as existing folder should throw an error', () => {
-    expect(() => createLogger({
-      output: 'file',
-      dir: path.join(__dirname, 'README.md')
-    })).toThrow()
-  })
-
-  it('logging to file should create the autorotate folder if it does not exist', () => {
-    const logger = createLogger({
       output: 'file',
       dir: path.join(__dirname, 'log')
     })
 
+    expect(fs.mkdirSync).toBeCalled()
+  })
+
+  it('logging with file output but with non-existent folder and parent folder should throw an error', () => {
+    fs.existsSync.mockReturnValueOnce(false)
+    fs.existsSync.mockReturnValueOnce(false)
+    fs.existsSync.mockReturnValueOnce(false)
+
+    expect(() => createLogger({
+      output: 'file',
+      dir: path.join(__dirname, 'parent', 'log')
+    })).toThrow(/no such directory or parent directory/)
+  })
+
+  it('logging with file output and with dir specified as existing file should throw an error', () => {
+    fs.existsSync.mockReturnValueOnce(true)
+    isFileStub.mockReturnValueOnce(true)
+
+    expect(() => createLogger({
+      output: 'file',
+      dir: path.join(__dirname, 'README.md')
+    })).toThrow(/is a file/)
+  })
+
+  it('logging to file should create the autorotate folder if it does not exist', () => {
+    const logger = createLogger({
+      output: 'file'
+    })
+
+    fs.existsSync.mockReturnValueOnce(false)
+    glob.sync.mockReturnValueOnce(Array(1).fill(''))
+    fs.mkdirSync.mockClear()
+
     logger('testing logger')
 
-    expect(fsMkdirSyncStub.called).toBe(true)
+    expect(fs.mkdirSync.mock.calls.length).toBe(1)
   })
 
   it('logging to file with autorotate disabled should not create the autorotate folder', () => {
     const logger = createLogger({
       output: 'file',
-      dir: path.join(__dirname, 'log'),
       autorotate: 'none'
     })
 
+    fs.existsSync.mockReturnValueOnce(true)
+    glob.sync.mockReturnValueOnce(Array(1).fill(''))
+    fs.mkdirSync.mockClear()
+
     logger('testing logger')
 
-    expect(fsMkdirSyncStub.called).toBe(false)
+    expect(fs.mkdirSync).not.toBeCalled()
   })
 
   it('logging to file with the file not existing should log to file with suffix `_1.log`', () => {
     const logger = createLogger({
       output: 'file',
-      dir: path.join(__dirname, 'log'),
       autorotate: 'none'
     })
 
+    fs.existsSync.mockReturnValueOnce(true)
+    glob.sync.mockReturnValueOnce([])
+    fs.createWriteStream.mockClear()
+
     logger('testing logger')
 
-    expect(createStreamStub.getCall(0).args[0]).toMatch(/(_1.log)$/)
+    expect(fs.createWriteStream.mock.calls[0][0]).toMatch(/(_1.log)$/)
   })
 
-  it('logging to file with a size limit and the previous file having reached the limit should call log to another file with an incremented number', () => {
+  it('logging to file with a size limit in `K` unit and the previous file having reached the limit should call log to another file with an incremented number', () => {
     const logger = createLogger({
       output: 'file',
-      dir: path.join(__dirname, 'log'),
       autorotate: 'none',
       dirDateFormat: 'DD-MMMM-YYYY',
       dateFormat: 'DD-MMMM-YYYYTHH',
       size: '10K'
     })
 
-    const filename = path.join(__dirname, 'log', moment().format('DD-MMMM-YYYYTHH') + '_1.log')
-
-    fs.writeFileSync(filename, crypto.randomBytes(10 * 1024))
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.statSync.mockReturnValueOnce({ size: 10 * 1024 })
+    glob.sync.mockReturnValueOnce(Array(5).fill(''))
+    fs.createWriteStream.mockClear()
 
     logger('testing logger')
 
-    expect(createStreamStub.getCall(0).args[0]).toMatch(/(_2.log)$/)
-
-    fs.unlinkSync(filename)
+    expect(fs.createWriteStream.mock.calls[0][0]).toMatch(/(_6.log)$/)
   })
 
-  it('logging to a file with size limit specified in form of gigabytes should write to the same file when its size is below the limit', () => {
+  it('logging to file with a size limit in `M` unit and the previous file having reached the limit should call log to another file with an incremented number', () => {
     const logger = createLogger({
       output: 'file',
-      dir: path.join(__dirname, 'log'),
+      autorotate: 'none',
+      dirDateFormat: 'DD-MMMM-YYYY',
+      dateFormat: 'DD-MMMM-YYYYTHH',
+      size: '5M'
+    })
+
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.statSync.mockReturnValueOnce({ size: 5 * 1024 * 1024 })
+    glob.sync.mockReturnValueOnce(Array(3).fill(''))
+    fs.createWriteStream.mockClear()
+
+    logger('testing logger')
+
+    expect(fs.createWriteStream.mock.calls[0][0]).toMatch(/(_4.log)$/)
+  })
+
+  it('logging to file with a size limit in `G` unit and the previous file having reached the limit should call log to another file with an incremented number', () => {
+    const logger = createLogger({
+      output: 'file',
       autorotate: 'none',
       dirDateFormat: 'DD-MMMM-YYYY',
       dateFormat: 'DD-MMMM-YYYYTHH',
       size: '1G'
     })
 
-    const filename = path.join(__dirname, 'log', moment().format('DD-MMMM-YYYYTHH') + '_1.log')
-
-    fs.writeFileSync(filename, crypto.randomBytes(50 * 1024))
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.statSync.mockReturnValueOnce({ size: 1024 ** 3 })
+    glob.sync.mockReturnValueOnce(Array(1).fill(''))
+    fs.createWriteStream.mockClear()
 
     logger('testing logger')
 
-    expect(createStreamStub.getCall(0).args[0]).toMatch(/(_1.log)$/)
-
-    fs.unlinkSync(filename)
+    expect(fs.createWriteStream.mock.calls[0][0]).toMatch(/(_2.log)$/)
   })
 
-  it('logging to a file with size limit specified in form of megabytes should write to the same file when its size is below the limit', () => {
+  it('logging to a file with size limit specified in `G` unit should write to the same file when its size is below the limit', () => {
     const logger = createLogger({
       output: 'file',
-      dir: path.join(__dirname, 'log'),
       autorotate: 'none',
       dirDateFormat: 'DD-MMMM-YYYY',
       dateFormat: 'DD-MMMM-YYYYTHH',
-      size: '10M'
+      size: '1G'
     })
 
-    const filename = path.join(__dirname, 'log', moment().format('DD-MMMM-YYYYTHH') + '_1.log')
-
-    fs.writeFileSync(filename, crypto.randomBytes(20 * 1024))
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.statSync.mockReturnValueOnce({ size: 0.87 * 1024 * 1024 * 1024 })
+    glob.sync.mockReturnValueOnce(Array(1).fill(''))
+    fs.createWriteStream.mockClear()
 
     logger('testing logger')
 
-    expect(createStreamStub.getCall(0).args[0]).toMatch(/(_1.log)$/)
+    expect(fs.createWriteStream.mock.calls[0][0]).toMatch(/(_1.log)$/)
+  })
 
-    fs.unlinkSync(filename)
+  it('logging to a file with size limit specified in `M` unit should write to the same file when its size is below the limit', () => {
+    const logger = createLogger({
+      output: 'file',
+      autorotate: 'none',
+      dirDateFormat: 'DD-MMMM-YYYY',
+      dateFormat: 'DD-MMMM-YYYYTHH',
+      size: '7M'
+    })
+
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.statSync.mockReturnValueOnce({ size: 6.4 * 1024 * 1024 })
+    glob.sync.mockReturnValueOnce(Array(8).fill(''))
+    fs.createWriteStream.mockClear()
+
+    logger('testing logger')
+
+    expect(fs.createWriteStream.mock.calls[0][0]).toMatch(/(_8.log)$/)
+  })
+
+  it('logging to a file with size limit specified in `K` unit should write to the same file when its size is below the limit', () => {
+    const logger = createLogger({
+      output: 'file',
+      autorotate: 'none',
+      dirDateFormat: 'DD-MMMM-YYYY',
+      dateFormat: 'DD-MMMM-YYYYTHH',
+      size: '200K'
+    })
+
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.existsSync.mockReturnValueOnce(true)
+    fs.statSync.mockReturnValueOnce({ size: 150 * 1024 })
+    glob.sync.mockReturnValueOnce(Array(50).fill(''))
+    fs.createWriteStream.mockClear()
+
+    logger('testing logger')
+
+    expect(fs.createWriteStream.mock.calls[0][0]).toMatch(/(_50.log)$/)
   })
 
   it('logging to file with level passed should write to the file with the level as part of the filename suffix in lowercase', () => {
     const logger = createLogger({
       output: 'file',
-      dir: path.join(__dirname, 'log'),
       autorotate: 'none'
     })
 
+    fs.existsSync.mockReturnValueOnce(true)
+    glob.sync.mockReturnValueOnce([])
+    fs.createWriteStream.mockClear()
+
     logger('testing logger', 'Warn')
 
-    expect(createStreamStub.getCall(0).args[0]).toMatch(/(_warn_1.log)$/)
+    expect(fs.createWriteStream.mock.calls[0][0]).toMatch(/(_warn_1.log)$/)
   })
 })
